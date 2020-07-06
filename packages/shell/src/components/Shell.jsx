c6fa450fad8a8e6059297b6ef3ae2cf12e407733
Bulent Gorkem <bgorkem@hotmail.com>
2020-07-06 17:25:04 +0100

    Basic state management

--- packages/shell/src/components/Shell.jsx
+++ packages/shell/src/components/Shell.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import createStore from '../state';
+
 import Home from './Home';
 import ReactSystemLoader from './ReactSystemLoader';
 import { Nav } from './Nav';
 import SvelteSystemLoader from './SvelteSystemLoader';
+import Preferences from './Preferences';
+
+const store = createStore();
 
 export default () => {
   return (
     <div>
-      <h2>MicroFE Shell</h2>
-      <Router>
-        <Nav />
-        <main>
-          <Switch>
-            <Route path="/app1">
-              <ReactSystemLoader app="app1" delay={100} />
-            </Route>
+      <Provider store={store}>
+        <h2>MicroFE Shell</h2>
+        <Router>
+          <Nav />
+          <Preferences />
+          <main>
+            <Switch>
+              <Route path="/app1">
+                <ReactSystemLoader app="app1" delay={100} />
+              </Route>
 
-            <Route path="/app2">
-              <SvelteSystemLoader app="app2" />
-            </Route>
+              <Route path="/app2">
+                <SvelteSystemLoader app="app2" />
+              </Route>
 
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </main>
-      </Router>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </Provider>
     </div>
   );
 };
